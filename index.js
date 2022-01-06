4333319aca854681aafe3817bd1be50ab78040b2
Viktor <vityaschel@utidteam.com>
2022-01-06 16:00:26 +0400

    Fix bug with nodejs

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-import nodefetch from 'node-fetch'
-
 async function getAvatars() {
-  const fetch = window?.fetch ?? nodefetch
+  let fetch, useWindowFetch = false
+  try { window.fetch } catch(e) { useWindowFetch = true }
+  if(useWindowFetch) fetch = (await import('node-fetch')).default
+  else fetch = window.fetch
+  console.log(await import('node-fetch'))
   const response = await fetch('https://randomavatar.com/more.php')
   const html = await response.text()
   const images = Array.from(html.matchAll(/(https:\/\/cdn\.cloudflare\.steamstatic\.com.*?)"/g)).map(img => img[1])
