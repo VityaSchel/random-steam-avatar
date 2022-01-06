101e7c14c47ad7f76e2f0e9dfb2c421f73ee622d
Viktor <vityaschel@utidteam.com>
2022-01-06 16:03:42 +0400

    Remove redunant console.log

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ async function getAvatars() {
   try { window.fetch } catch(e) { useWindowFetch = true }
   if(useWindowFetch) fetch = (await import('node-fetch')).default
   else fetch = window.fetch
-  console.log(await import('node-fetch'))
   const response = await fetch('https://randomavatar.com/more.php')
   const html = await response.text()
   const images = Array.from(html.matchAll(/(https:\/\/cdn\.cloudflare\.steamstatic\.com.*?)"/g)).map(img => img[1])
