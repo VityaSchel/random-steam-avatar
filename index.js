d56cecb939a8eef3532a1f677e0469ad4de1e95f
Viktor <vityaschel@utidteam.com>
2022-01-06 16:29:43 +0400

    Protect from cors

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 async function getAvatars() {
   let fetch, useWindowFetch = true
   try { window.fetch } catch(e) { useWindowFetch = false }
-  if(useWindowFetch) fetch = (await import('node-fetch')).default
-  else fetch = window.fetch
-  const response = await fetch('https://randomavatar.com/more.php')
+  if(useWindowFetch) fetch = window.fetch
+  else fetch = (await import('node-fetch')).default
+  let response
+  try { response = await fetch('https://randomavatar.com/more.php') } catch(e) {
+    response = await fetch('https://api.allorigins.win/raw?url=https://randomavatar.com/more.php')
+  }
   const html = await response.text()
   const images = Array.from(html.matchAll(/(https:\/\/cdn\.cloudflare\.steamstatic\.com.*?)"/g)).map(img => img[1])
   // I match html with regex yes go fuck yourself
